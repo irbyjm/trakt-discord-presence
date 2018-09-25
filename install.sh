5a3639664380d7ae0d7bc4ce2e8ffdc5c8a75b66
irbanoz <irbyjm@gmail.com>
2018-09-24 23:13:15 -0400

    ran into a dep ordering issue after doing an upgrade

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # install npm dependencies
 # it doesn't like the current version of uws
-npm install erlpack@discordapp/erlpack bufferutil@^3.0.0 libsodium-wrappers@^0.7.0 sodium@^2.0.0 uws@^9.14.0 zlib-sync@^0.1.0 @discordjs/uws@^10.149.0 bufferutil@^4.0.0 register-scheme@github:devsnek/node-register-scheme
+npm install erlpack@discordapp/erlpack bufferutil@^3.0.0 bufferutil@^4.0.0 libsodium-wrappers@^0.7.0 sodium@^2.0.0 uws@^9.14.0 zlib-sync@^0.1.0 @discordjs/uws@^10.149.0 register-scheme@github:devsnek/node-register-scheme
 
 # install total package
 npm install
